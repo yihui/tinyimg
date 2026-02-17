a1c38566aca163f21ad07b4eb035ade64689e5ba
Yihui Xie <xie@yihui.name>
2026-02-17 00:02:12 -0600

    too much technical detail

--- R/tinyimg-package.R
+++ R/tinyimg-package.R
@@ -1,28 +1,7 @@
 #' tinyimg: Optimize and Compress Images
 #'
-#' The tinyimg package provides tools for optimizing and compressing images using
-#' Rust libraries. Currently supports PNG optimization via the oxipng crate.
-#'
-#' ## Main Functions
-#'
-#' - [optim_png()]: Optimize PNG images without quality loss
-#'
-#' ## Optimization Levels
-#'
-#' The package supports optimization levels from 0 to 6:
-#'
-#' - **Level 0**: Fastest, minimal compression
-#' - **Level 2**: Default, balanced speed and compression
-#' - **Level 6**: Maximum compression, slower
-#'
-#' @section Author:
-#' Yihui Xie
-#'
-#' @section See Also:
-#' Useful links:
-#' - <https://github.com/yihui/tinyimg>
-#' - Report bugs at <https://github.com/yihui/tinyimg/issues>
-#'
+#' The tinyimg package provides tools for optimizing and compressing images
+#' using Rust libraries.
 #' @keywords internal
 #' @useDynLib tinyimg, .registration = TRUE
 "_PACKAGE"
