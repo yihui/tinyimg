c6169908ddeb4620140d9b6ac961950c88c1f948
Yihui Xie <xie@yihui.name>
2026-04-15 14:21:45 -0500

    a better default example

--- playground/_default.R
+++ playground/_default.R
@@ -1,7 +1,18 @@
 # For more info, see https://pkg.yihui.org/tinyimg/
-tmp = tempfile(fileext = '.png')
-png(tmp, width = 400, height = 400)
-plot(1:10)
-dev.off()
 
-tinypng::tinyimg(tmp)
+test_plot = function(expr, filename = tempfile(fileext = '.png'), ...) {
+  png(filename, ...)
+  expr
+  dev.off()
+  filename
+}
+
+#| results = 'asis'
+out = test_plot({
+  par(mar = c(4, 4, 1, .1))
+  with(penguins, plot(bill_len ~ bill_dep, pch = 19, col = species))
+})
+xfun::html_tag("img", src = xfun::base64_uri(out), alt = "Original")
+
+tinyimg::tinypng(out)
+xfun::html_tag("img", src = xfun::base64_uri(out), alt = "Optimized")
